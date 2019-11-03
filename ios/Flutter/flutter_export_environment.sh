e066ed55dd9beca9ce7d9eee739f9c9384a2cec8
Joo <joo@joostory.net>
2019-11-03 17:51:45 +0900

    pod

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/joo.yoon/Applications/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/joo.yoon/workspace/mobile/oh-my-bible"
+export "FLUTTER_TARGET=/Users/joo.yoon/workspace/mobile/oh-my-bible/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/joo.yoon/Applications/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=2.2.1"
+export "FLUTTER_BUILD_NUMBER=25"
+export "TRACK_WIDGET_CREATION=true"
